61af83e37d0d3028d55b05ab5258e1318d687995
AkshayG <akshayg14@gmail.com>
2020-10-16 21:06:01 +0530

    Added bcrypt and storage of registered user detail

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require('express');
+const bcrypt = require('bcrypt'); // for encryption purpose
 const app = express();
 
-const users = []; // variable to save the registered users
+const users = []; // variable to save the registered users... as it's local declared, it will be reset on every reload
 
 app.set('view-engine', 'ejs');
 
@@ -24,7 +25,20 @@ app.get('/register', (req, res)=> {
   res.render('register.ejs');
 });
 
-app.post('/register', (req,res)=> {
+app.post('/register', async (req,res)=> {
+  try {
+    const hashedPassword = await bcrypt.hash(req.body.password, 10);
+    users.push({
+      id: Date.now().toString(),
+      name: req.body.name,
+      email: req.body.email,
+      password: hashedPassword
+    });
+    console.log(users);
+    res.redirect('/login');
+  } catch{
+    res.redirect('/register');
+  }
 });
 
 app.listen(3000);
