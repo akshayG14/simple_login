dbd26a2a33133037c7321f7914d0024814108a1a
AkshayG <akshayg14@gmail.com>
2020-10-16 20:12:55 +0530

    Added the option to store registered users and access to form values

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 const express = require('express');
 const app = express();
 
+const users = []; // variable to save the registered users
+
 app.set('view-engine', 'ejs');
+
+// allow the form values to be accessed in the req variable in post
+app.use(express.urlencoded({extended: false}));
 
 app.get('/', (req, res)=> {
   res.render('index.ejs', {name: 'Ashok Kumar'});
@@ -11,8 +16,15 @@ app.get('/login', (req, res)=> {
   res.render('login.ejs');
 });
 
+app.post('/login', (req,res)=> {
+  
+});
+
 app.get('/register', (req, res)=> {
   res.render('register.ejs');
+});
+
+app.post('/register', (req,res)=> {
 });
 
 app.listen(3000);
